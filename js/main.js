c18cd5a819f43170fe668fde6a8e501e18372403
Maka Rousse <makarousse@gmail.com>
2019-08-19 13:12:24 -0700

    opacity scrollingDown
    opacity at scrollingDown
    chanded color of buttom submit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+/* callback function */
+/*
+$(function() {
+
+  setTimeout(function() {
+    $("#arrowDown").hide();
+  }, 3000);
+
+});
+
+
+
+
+/*Scroll down when arrow up clicked BEGIN
+
+
+/*
+THIS SUPPOSE TO  FADE OUT AT SCROLLING
+STILL WORKING AT IT --->>>*/
+
+
+
+$(window).scroll(function() {
+  $("#arrowDown").css("opacity", 1 - $(window).scrollTop() / 550);
+});
+/*Scroll to top when arrow up clicked END*/
+
+
+
+
+
+
+
+
 //var quien = prompt("A quien ama makita?");
 //alert(quien); //
 
